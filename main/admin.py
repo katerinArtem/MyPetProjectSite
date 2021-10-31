a6de8b6c95e21cdc978b9d9154ab7ea36787c1f8
katerinArtem <playdotamysun@gmail.com>
2021-10-31 03:51:33 +0300

    Message added in admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import CustomUser,Post
+from .models import CustomUser,Post,Message
 #from .models import User
 # Register your models here.
 #admin.site.register(User) 
 admin.site.register(CustomUser)
 admin.site.register(Post)
+admin.site.register(Message)
