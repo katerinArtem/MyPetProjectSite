b74edae2ded59ddd021e3dfcb95f1042888e0cda
katerinArtem <playdotamysun@gmail.com>
2021-11-05 05:56:59 +0300

    js for posts

---
+++ static/js/posts.js
@@ -0,0 +1,5 @@
+let $infinite_container = $('.infinite_container').infiniteScroll({
+    path: '.infinite-more-link',
+    append: '.infinite-item',
+    });
+let infScroll = $infinite_container.data('infiniteScroll');
