aca577d854e8be33e666d75a3b1360f019555be9
katerinArtem <playdotamysun@gmail.com>
2021-11-06 04:41:50 +0300

    added Content.jsx use code from base.html

---
+++ frontend/src/components/content/Content.jsx
@@ -0,0 +1,20 @@
+import React, {useState,useEffect} from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../../styles/content/Content.css"
+
+const Content = () => {
+  return (
+    <div className="background_content">
+        <hr/>
+        <hr/>
+        <hr/>
+        <hr/>
+        <hr/>
+        <hr/>
+        <strong>Content</strong>  
+    </div>
+  );
+}
+
+export default Content;
