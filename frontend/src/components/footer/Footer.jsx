e6b96c296a19cc6a3da3d8f496bccb406932d34e
katerinArtem <playdotamysun@gmail.com>
2021-11-06 04:41:17 +0300

    Added Footer.jsx use code from base.html

---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React, {useState,useEffect} from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../../styles/footer/Footer.css"
+
+const Footer = () => {
+  return (
+    <div className="footer_color">
+        <a href="#"><h2>some footer some footer</h2></a>  
+    </div>
+  );
+}
+
+export default Footer;
